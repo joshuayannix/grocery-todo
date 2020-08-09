2800d7fe75182ef4d611e2889c8030f94aa1a315
Joshua Yannix <joshuayannix@gmail.com>
2020-08-09 14:53:01 -0500

    functionality complete

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const addItem = e => {
   e.preventDefault()
   const value = grocery.value;
   const id = new Date().getTime().toString()
-  if(value && !editFlag){
+  if (value && !editFlag) {
     // if completely new item, and not editing
     const element = document.createElement('article');
     // ad class
@@ -46,12 +46,16 @@ const addItem = e => {
     //show container
     container.classList.add('show-container')
     // add to local storage
-    addToLocalStorage(id,value);
+    addToLocalStorage(id, value);
     // set back to default
     setBackToDefault()
-  } else if(value && editFlag){
+  } else if (value && editFlag) {
     // if editing a current item
-    console.log('edit');
+    editElement.innerHTML = value;
+    displayAlert('successfully edited!', 'success');
+    // edit local storage
+    editLocalStorage(editID, value)
+    setBackToDefault();
   } else {
     // if user has not added any value
     displayAlert('please enter value', 'danger')
@@ -59,7 +63,7 @@ const addItem = e => {
 }
 
 // display alert
-const displayAlert = (text,action) => {
+const displayAlert = (text, action) => {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
 
@@ -73,7 +77,7 @@ const displayAlert = (text,action) => {
 const clearItems = () => {
   const items = document.querySelectorAll('.grocery-item')
   console.log(items);
-  if(items.length > 0){
+  if (items.length > 0) {
     items.forEach(item => {
       list.removeChild(item)
     })
@@ -81,17 +85,34 @@ const clearItems = () => {
   container.classList.remove('show-container')
   displayAlert('empty list', 'success');
   setBackToDefault()
-    // localStorage.removeItem('list')
+  // localStorage.removeItem('list')
 }
 // delete function
 const deleteItem = e => {
   const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id
+  console.log(id)
   list.removeChild(element)
+  if (list.children.length === 0) {
+    container.classList.remove('show-container');
+  }
+  displayAlert('item removed', 'danger')
+  setBackToDefault();
+  // remove from local storage
+  // removeFromLocalStorage(id);
 }
 
 // edit function
 const editItem = e => {
-  console.log('item edited');
+  // the actual article element with class grocery-item
+  const element = e.currentTarget.parentElement.parentElement;
+  // set edit item - edit the title
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  // set the value in the input form to equal the title of item we're editing
+  grocery.value = editElement.innerHTML;
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = 'edit'
 }
 
 // set back to default
@@ -115,4 +136,13 @@ const deleteBtn = document.querySelector('.delete-btn');
 const addToLocalStorage = (id, value) => {
   console.log('added to local storage')
 }
+
+const removeFromLocalStorage = id => {
+
+}
+
+const editLocalStorage = (id, value) => {
+
+}
+
 // ****** SETUP ITEMS **********
